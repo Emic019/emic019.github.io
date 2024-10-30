24b9471266e2473564318a684aa182d103bf04ad
Your Name <guting@GTdeMacBook-Pro.local>
2024-10-31 00:52:58 +0800

    Site updated: 2024-10-31 00:52:58

--- bundle.js
+++ bundle.js
@@ -970,6 +970,24 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 ;
+/* global NexT, CONFIG */
+
+document.addEventListener('page:loaded', () => {
+  if (!CONFIG.page.comments) return;
+
+  NexT.utils.loadComments('.utterances-container')
+    .then(() => NexT.utils.getScript('https://utteranc.es/client.js', {
+      attributes: {
+        async       : true,
+        crossOrigin : 'anonymous',
+        'repo'      : CONFIG.utterances.repo,
+        'issue-term': CONFIG.utterances.issue_term,
+        'theme'     : CONFIG.utterances.theme
+      },
+      parentNode: document.querySelector('.utterances-container')
+    }));
+});
+;
 $(document).ready(function() {
 	
 	categoryUnfold();
